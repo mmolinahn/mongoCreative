24f4eb8c736a21f8647e0c6c9227142c11ccccd9
mmolinahn <molina.moisesa@gmail.com>
2017-11-17 14:47:45 -0700

    Update index.js
    tweek

--- dateIdeas/routes/index.js
+++ dateIdeas/routes/index.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-var mongoose = require('mongoose'); //Adds mongoose as a usable dependency
-mongoose.connect('mongodb://localhost/dateDB'); //Connects to a mongo database called "commentDB"
+var mongoose = require('mongoose'); 
+mongoose.connect('mongodb://localhost/dateDB'); 
 var dateSchema = mongoose.Schema({
 Budget: String,
 Activity: String
 });
-var Dates = mongoose.model('Dates', dateSchema); //Makes an object from that schema as a model
-var db = mongoose.connection; //Saves the connection as a variable to use
-db.on('error', console.error.bind(console, 'connection error:')); //Checks for connection errors
-db.once('open', function() { //Lets us know when we're connected
+var Dates = mongoose.model('Dates', dateSchema); 
+var db = mongoose.connection; 
+db.on('error', console.error.bind(console, 'connection error:')); 
+db.once('open', function() { 
 console.log('Connected');
 });
 
@@ -24,8 +24,8 @@ router.get('/date', function(req, res, next) {
 
 });
 router.post('/date', function(req, res, next) {
-  var newdate = new Dates(req.body); //[3]
-  newdate.save(function(err, post) { //[4]
+  var newdate = new Dates(req.body); 
+  newdate.save(function(err, post) { 
   if (err) return console.error(err);
    res.sendStatus(200);
   })
